ae2ae4f59b81ac8e76f2e993042a1421ae8c6d71
Smit Shah <smit.shah@teknopoint.in>
2024-07-12 17:06:56 +0530

    .html added on city-list

--- blocks/city-list/city-list.js
+++ blocks/city-list/city-list.js
@@ -1,20 +1,27 @@
 import { fetchAPI, getProps, renderHelper } from "../../scripts/scripts.js";
 
 export default async function decorate(block) {
-    const [url] = getProps(block);
-    const resp = await fetchAPI("GET", url);
-    const data = await resp.json();
+  const [url] = getProps(block);
+  const resp = await fetchAPI("GET", url);
+  const data = await resp.json();
 
-    let li = renderHelper(data.data.sort(function (a, b) {
-        if (a.text < b.text) {
-            return -1;
-        }
-        if (a.text > b.text) {
-            return 1;
-        }
-        return 0;
-    }), `<div class="forName">
+  let li = renderHelper(
+    data.data.sort(function (a, b) {
+      if (a.text < b.text) {
+        return -1;
+      }
+      if (a.text > b.text) {
+        return 1;
+      }
+      return 0;
+    }),
+    `<div class="forName">
         <li><a href="{url}" title="{text}">{text}</a></li>    
-    </div>`)
-    block.innerHTML = '<ul>' + li + '</ul>'
+    </div>`,
+    function (data) {
+      data.url = data.url.endsWith(".html") ? data.url : data.url + ".html";
+      return data;
+    }
+  );
+  block.innerHTML = "<ul>" + li + "</ul>";
 }
