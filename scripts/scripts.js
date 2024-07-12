fc5ff896a1181a1cb163be008d9bfec52223923c
Om Gupta <om.gupta@teknopoint.in>
2024-07-12 21:43:13 +0530

    stage author url added in main  branch

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@ import { toggleAllNavSections } from "../blocks/header/header.js";
 import { sampleRUM, loadHeader, loadFooter, decorateButtons, decorateIcons, decorateSections, decorateBlocks, decorateTemplateAndTheme, waitForLCP, loadBlocks, loadCSS } from "./aem.js";
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
-console.log("Main Branch 1.2");
+console.log("Main Branch 1.3");
 /**
  * Moves all the attributes from a given elmenet to another given element.
  * @param {Element} from the element to copy attributes from
